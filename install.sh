48b10cce7f61f5873e4fa856aad739c7e966447b
Henry Mackay <hmm.mackay@gmail.com>
2022-10-14 15:38:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,10 +15,20 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt-get update
   apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-  
-  printf "Enter desired Path for design folder: "
-  
-  read -r design_PATH
+
+got_path = 1;
+
+while  ! [ got_path ]; do 
+   printf "Enter desired Path for Design folder: "
+
+    read -r design_PATH
+
+    if ! [ -d "$design_PATH" ]; then
+      echo "ERROR!,  ${design_PATH}... DOES NOT EXIST"
+    else 
+       got_path=0;
+     fi
+done
   
   mkdir $design_PATH
  
@@ -34,7 +44,32 @@ docker pull efabless/foss-asic-tools:latest
 clear 
 echo " #### Installing MPW Precheck in home #### "
 sleep 2
-mkdir 
+sudo apt install git make docker.io pip
+clear
+
+got_path = 1;
+while  ! [ got_path ]; do 
+    printf "Enter desired Path for Precheck installation folder: "
+
+    read -r precheck_PATH
+
+    if ! [ -d "$precheck_PATH" ]; then
+      echo "ERROR!,  ${precheck_path}... DOES NOT EXIST"
+    else 
+       got_path=0;
+     fi
+done
+
+cd precheck_PATH
+git clone -b mpw-7a https://github.com/efabless/caravel_user_project_analog
+cd caravel_user_project_analog 
+make install
+
+cd precheck_PATH
+
+
+
+
 
 
  
