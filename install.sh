8673f678c4f2bd931a4dbc6db0746bc955d532b5
Henry Mackay <hmm.mackay@gmail.com>
2022-10-14 16:43:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #Docker dependencies
-apt-get remove docker docker-engine docker.io containerd runc
-apt-get update
-apt-get install \
+apt-get -y remove docker docker-engine docker.io containerd runc
+apt-get -y update
+sudo apt-get -y install tigervnc-viewer
+apt-get -y install \
     ca-certificates \
     curl \
     gnupg \
@@ -13,8 +14,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt-get update
-  apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  apt-get -y update
+  apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin gksu
 
 got_path = 1;
 
@@ -40,11 +41,16 @@ sleep 2
   
 docker pull efabless/foss-asic-tools:latest
   
+  echo "## Adding shortcut on desktop... ###"
+  printf "\nEnter username of non root main user: "
+  read -r name
+  cp ./EFABLESS /home/$name/Desktop
+  chmod +x /home/$name/Desktop
   
 clear 
 echo " #### Installing MPW Precheck in home #### "
 sleep 2
-sudo apt install git make docker.io pip
+sudo apt -y install git make pip
 clear
 
 got_path = 1;
@@ -65,7 +71,10 @@ git clone -b mpw-7a https://github.com/efabless/caravel_user_project_analog
 cd caravel_user_project_analog 
 make install
 
-cd precheck_PATH
+
+
+
+
 
 
 
