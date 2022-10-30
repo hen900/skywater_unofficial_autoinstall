976889a83073122b02278a28adce7aef610e790e
Henry Mackay <hmm.mackay@gmail.com>
2022-10-30 18:15:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -105,6 +105,8 @@ git clone -b mpw-7a https://github.com/efabless/caravel_user_project_analog
 cd caravel_user_project_analog 
 sed -i "1i export PDK_ROOT?=/$precheck_PATH/pdks/" Makefile
 make install
+reboot () { echo 'To complete the installation, you must Reboot. Would you like to reboot now? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot; }
+
 printf "\n\n\n##### DONE #####\n"
 
 
